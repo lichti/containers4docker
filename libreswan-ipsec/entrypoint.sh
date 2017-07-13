919befbf005dbe253781e8268cab4611d29d9e9e
Lichti <gustavo.lichti@gmail.com>
2017-07-13 15:14:46 -0300

    change / per | into sed command

--- libreswan-ipsec/entrypoint.sh
+++ libreswan-ipsec/entrypoint.sh
@@ -1,17 +1,17 @@
-#!/bin/bash
+#!/bin/sh
 
-sed -i "s/{{conn_name}}/$CNAME/g" /tmp/ipsec.conf 
-sed -i "s/{{local-pip}}/$LPIP/g" /tmp/ipsec.conf
-sed -i "s/{{local-cidr}}/$LCIDR/g" /tmp/ipsec.conf
+sed -i "s|{{conn_name}}|$CNAME|g" /tmp/ipsec.conf 
+sed -i "s|{{local-pip}}|$LPIP|g" /tmp/ipsec.conf
+sed -i "s|{{local-cidr}}|/$LCIDR|g" /tmp/ipsec.conf
 
-sed -i "s/{{remote-pip}}/$RPIP/g" /tmp/ipsec.conf
-sed -i "s/{{remote-cidr}}/$RCIDR/g" /tmp/ipsec.conf
+sed -i "s|{{remote-pip}}|$RPIP|g" /tmp/ipsec.conf
+sed -i "s|{{remote-cidr}}|$RCIDR|g" /tmp/ipsec.conf
 
-sed -i "s/{{local-pip}}/$LPIP/g" /tmp/ipsec.secrets
-sed -i "s/{{remote-pip}}/$RPIP/g" /tmp/ipsec.secrets
-sed -i "s/{{psk}}/$PSK/g" /tmp/ipsec.secrets
+sed -i "s|{{local-pip}}|$LPIP|g" /tmp/ipsec.secrets
+sed -i "s|{{remote-pip}}|$RPIP|g" /tmp/ipsec.secrets
+sed -i "s|{{psk}}|$PSK|g" /tmp/ipsec.secrets
 
 mv /tmp/ipsec.conf /etc/ipsec.d/$CNAME.conf
 mv /tmp/ipsec.secrets /etc/ipsec.d/$CNAME.secrets
 
-bash
+/bin/sh
